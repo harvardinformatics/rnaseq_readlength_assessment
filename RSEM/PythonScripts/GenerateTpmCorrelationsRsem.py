b76fc52876b19684b88dfdc1db3236dddbb008a9
afreedman <adamfreedman@fas.harvard.edu>
2019-09-18 10:53:41 -0400

    cleanup rsem tpm cor code

--- RSEM/PythonScripts/GenerateTpmCorrelationsRsem.py
+++ RSEM/PythonScripts/GenerateTpmCorrelationsRsem.py
@@ -46,35 +46,25 @@ if __name__=="__main__":
     fout=open('%s_rsem_tpmcorrelations_by_strategy_%s.tsv' % (level,opts.sra),'w')
     fout.write('%s\tsample\tstrategy\tspearmanr\tpval\n' % opts.sra)
  
-    #strategies = ['2x40','1x125','1x75']
-    
     pe_results = glob.glob('RSEM*[40,125]*%s*.results' % level)
-    print 'pe results', pe_results
     se_results = glob.glob('se*[125,75]*%s*.results' % level)
-    print 'se results',se_results
+    
     if level == 'isoform':
         samples = samples=Set(['_'.join(i.replace('.isoforms.results','').split('_')[2:-1]) for i in pe_results])
     else:
         samples = samples=Set(['_'.join(i.replace('.genes.results','').split('_')[2:-1]) for i in pe_results])
     
     for sample in samples:
-        print sample
-        #KO_Inf_rep3_125
-        #RSEM_bt2_KO_Inf_rep1_125_adapt.genes.results
         paired125 = glob.glob('RSEM*%s_125*%s.results' % (sample,level))
-        print 'paired 125',paired125
         p125_dict = rsem_parser(paired125[0],gene=opts.gene)
 
         paired40 = glob.glob('RSEM*%s_40*%s.results' % (sample,level))    
-        print 'paired 40',paired40
         p40_dict = rsem_parser(paired40[0],gene=opts.gene)
 
         se125 = glob.glob('se*%s_125*%s.results' % (sample,level)) 
-        print 'se125',se125
         s125_dict = rsem_parser(se125[0],gene=opts.gene)
 
         se75 = glob.glob('se*%s_75*%s.results' % (sample,level))
-        print 'se75', se75
         s75_dict = rsem_parser(se75[0],gene=opts.gene)    
 
         p40_cor,p40_p = CalcSpearman(p125_dict,p40_dict)
